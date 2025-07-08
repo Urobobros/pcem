b36fcc0f661a38c9701aaf3b062e12a3974d6c99
Urobobros <martin.burianek1987@gmail.com>
2025-07-08 20:09:20 +0200

    Improve CPU logging helper

---
+++ includes/private/cpu_debug.h
@@ -0,0 +1,14 @@
+#ifndef PCEM_CPU_DEBUG_H
+#define PCEM_CPU_DEBUG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void cpu_log_state(const char *prefix);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PCEM_CPU_DEBUG_H */
---
+++ src/cpu/cpu_debug.c
@@ -0,0 +1,26 @@
+#include "ibm.h"
+#include "x86.h"
+#include "cpu.h"
+#include <pcem/logging.h>
+#include <stdlib.h>
+
+static int log_count = 0;
+static int log_limit = -1;
+
+void cpu_log_state(const char *prefix) {
+#ifndef RELEASE_BUILD
+        if (log_limit == -1) {
+                const char *env = getenv("CPU_LOG_LIMIT");
+                if (env)
+                        log_limit = atoi(env);
+                else
+                        log_limit = 100;
+        }
+
+        if (log_limit >= 0 && log_count >= log_limit)
+                return;
+        log_count++;
+        pclog("%s PC=%08X CS=%04X EAX=%08X EBX=%08X ECX=%08X EDX=%08X ESI=%08X EDI=%08X ESP=%08X EBP=%08X EFLAGS=%08X\n", prefix,
+              cpu_state.pc, CS, EAX, EBX, ECX, EDX, ESI, EDI, ESP, EBP, cpu_state.eflags);
+#endif
+}
